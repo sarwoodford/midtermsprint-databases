dac6d3b336310f0ca04bf90587231426bafcabed
sara woodford <sarawoodford6@gmail.com>
2024-10-22 16:47:49 -0230

    inserting data
    inserting data into each table, ensuring there are
    5 customers, 5 movies and 10 rentals as per the
    assignment outline.

--- postgreSQL/Movies.sql
+++ postgreSQL/Movies.sql
@@ -25,3 +25,32 @@ movie_id INT REFERENCES movies(movie_id),
 rental_date DATE NOT NULL,
 return_date DATE
 );
+
+-- INSERT DATA INTO TABLES, ENSURING THERE ARE (at least) 5 MOVIES, 5
+-- CUSTOMERS, AND 10 RENTALS 
+
+INSERT INTO movies (title, release_year, genre, director_name) VALUES
+('Forrest Gump', 1994, 'Comedy', 'Robert Zemeckis'),
+('The Godfather', 1972, 'Crime', 'Francis Ford Coppola'),
+('Whiplash', 2014, 'Drama', 'Damien Chazelle'),
+('Shutter Island', 2010, 'Psychological Thriller', 'Martin Scorsese'),
+('Jurrastic Park', 1993, 'Sci-Fi', 'Steven Spielberg');
+
+INSERT INTO customers (first_name, last_name, email, phone_num) VALUES
+('John', 'Doe', 'jdoe@example.com', '1234567890'),
+('Jane', 'Smith', 'jsmith@example.com', '2345678901'),
+('Anna', 'Brown', 'abrown@example.com', '3456789012'),
+('Bob', 'Roberts', 'broberts@example.com', '4567890123'),
+('Lisa', 'James', 'ljames@example.com', '5678901234');
+
+INSERT INTO rentals (customer_id, movie_id, rental_date, return_date) VALUES
+(1, 1, '2024-01-10', '2024-01-15'),
+(2, 2, '2024-02-09', '2024-02-20'),
+(3, 3, '2024-03-08', '2024-03-11'),
+(4, 4, '2024-04-07', '2024-04-14'), 
+(5, 5, '2024-05-06', '2024-05-08'), 
+(1, 3, '2024-06-05', '2024-06-07'), 
+(2, 1, '2024-07-04', '2024-07-08'),
+(3, 4, '2024-08-03', '2024-08-11'), 
+(4, 2, '2024-09-02', NULL), 
+(5, 3, '2024-10-01', NULL); 
